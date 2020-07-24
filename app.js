aa6e46aece581d6bd7805489f29768253d1dafff
Sayan Manna <53704744+Sayan-Manna@users.noreply.github.com>
2020-07-24 20:52:30 +0530

    DOM: Events and Event Handling the dice

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice;
-
+var scores, roundScore, activePlayer;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
@@ -17,14 +16,34 @@ activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1; // rolls from 1 to 6
 
 //Setter
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
 //Getter
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+    // 1, Random Number
+    var dice = Math.floor(Math.random() * 6) + 1
+
+    // 2. Display the Result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+
+    //3. Update the round IF the rolled number was NOT a 1
+
+
+
+});
 
 
 
